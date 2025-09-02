288ef0c5d956984e7a00c3e7030cc013054aa14c
tamoshka <127463024+tamoshka@users.noreply.github.com>
2025-09-02 19:08:40 +0700

    Add files via upload

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+print("Hello world")
+from flask import Flask
+app = Flask(__name__)
+########## ### ###### ######## ## #########
+@app.route("/")
+def hello():
+ return " <html><head></head> <body> Hello World! </body></html>"
+if __name__ == "__main__":
+ app.run(host='127.0.0.1',port=5000) 
