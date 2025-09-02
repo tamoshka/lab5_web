e05269bdd32adce7bd9d28a02d7a0f1c1346fb5b
tamoshka <127463024+tamoshka@users.noreply.github.com>
2025-09-02 20:27:51 +0700

    Update st.bat

--- flaskapp/st.bat
+++ flaskapp/st.bat
@@ -1,16 +1,8 @@
-@echo off
-echo Starting Gunicorn server...
-start /B gunicorn --bind 127.0.0.1:5000 wsgi:app
-
-timeout /t 5 /nobreak
-
+gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
+sleep 5
 echo start client
-python client.py
-
-timeout /t 5 /nobreak
-
-echo Killing Gunicorn process...
-taskkill /f /im gunicorn.exe
-taskkill /f /im python.exe
-
-exit /b 0
+python3 client.py
+sleep 5
+echo $APP_PID
+kill -TERM $APP_PID
+exit 0 
