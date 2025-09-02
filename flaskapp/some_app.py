c133620c484818e88c44be2e61963f8a43585cdc
tamoshka <127463024+tamoshka@users.noreply.github.com>
2025-09-02 20:01:55 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -74,3 +74,45 @@ def net():
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети, если был нажат сабмит, либо передадим falsy значения
  return render_template('net.html',form=form,image_name=filename,neurodic=neurodic) 
+
+from flask import request
+from flask import Response
+83
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+# метод для обработки запроса от пользователя
+@app.route("/apinet",methods=['GET', 'POST'])
+def apinet():
+ neurodic = {}
+ # проверяем, что в запросе json данные
+ if request.mimetype == 'application/json':
+ # получаем json данные
+ data = request.get_json()
+ # берем содержимое по ключу, где хранится файл
+ # закодированный строкой base64
+ # декодируем строку в массив байт, используя кодировку utf-8
+ # первые 128 байт ascii и utf-8 совпадают, потому можно
+ filebytes = data['imagebin'].encode('utf-8')
+ # декодируем массив байт base64 в исходный файл изображение
+ cfile = base64.b64decode(filebytes)
+ # чтобы считать изображение как файл из памяти, используем BytesIO
+ img = Image.open(BytesIO(cfile))
+ decode = neuronet.getresult([img])
+ neurodic = {}
+ for elem in decode:
+ neurodic[elem[0][1]] = str(elem[0][2])
+ print(elem)
+ # пример сохранения переданного файла
+ # handle = open('./static/f.png','wb')
+ # handle.write(cfile)
+ # handle.close()
+ # преобразуем словарь в json-строку
+ ret = json.dumps(neurodic)
+ # готовим ответ пользователю
+ resp = Response(response=ret,
+ status=200,
+ mimetype="application/json")
+ # возвращаем ответ
+ return resp 
